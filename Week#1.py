f315bfc4cd0f8a05d5fdb1b2c6df9a9435bc3102
Abdulhameed Nahhas <hmedo@Abdulhameeds-MacBook-Pro.local>
2019-08-24 12:35:28 +0300

    0.1

---
+++ Week#1.py
@@ -0,0 +1,18 @@
+"""----------Day1----------"""
+#How to print
+print (" Hello, World !")
+#How to exit
+exit()
+#must be space befor print
+if 4>2:
+ print("hmedo")
+"""----------Day2---------"""
+ #Use hashtag(#) to write a comment
+ print("Hi !") #This is a comment
+ #or to disable a command
+ #print("Hi !")
+ """for multi lines like this comment
+use " three times at the beginning
+and three times at the end
+of the command"""
+"""----------Day3----------"""
