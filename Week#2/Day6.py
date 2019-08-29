04911ed87fc93b4ab63a074858db11d120d7f51d
Abdulhameed Nahhas <hmedo@Abdulhameeds-MacBook-Pro.local>
2019-08-29 13:58:34 +0300

    0.8

---
+++ Week#2/Day6.py
@@ -0,0 +1,13 @@
+"""----------Day6----------"""
+#convert to integer
+A , B = int(3.9) , int("2")
+print(A)
+print(B)
+#convert to float
+C , D = float(3) , float("4.5")
+print(C)
+print(D)
+#convert to string
+x , y = str("hi i'm N.O. 1") , str(2.2)
+print(x)
+print(y)
